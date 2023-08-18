eb3547668ca950752968e18863d6a9444e03e027
Andre0Luis <aluis283@gmail.com>
2023-08-18 20:26:06 -0300

    Primeiro Commit

---
+++ src/main/java/br/com/classcheckersed/ClassCheckerSedApplication.java
@@ -0,0 +1,13 @@
+package br.com.classcheckersed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassCheckerSedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassCheckerSedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/classcheckersed/ClassCheckerSedApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.classcheckersed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassCheckerSedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
